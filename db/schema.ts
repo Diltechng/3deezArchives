cde7fe54fbe128ad912eaef9861590752a82d2fd
Ghali Musa <ghalimusa53@gmail.com>
2026-05-04 21:46:49 +0100

    feat: add fields for email verification flow to users

--- db/schema.ts
+++ db/schema.ts
@@ -3,11 +3,17 @@ import { pgTable, uuid, text, timestamp, boolean } from "drizzle-orm/pg-core";
 /**
  * Users table
  */
+const timestamps = {
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow()
+}
+
 export const users = pgTable("users", {
   id: uuid("id").defaultRandom().primaryKey(),
   email: text("email").notNull().unique(),
   name: text("name").notNull(),
+  passwordHash: text("password_hash"),
+  isVerified: boolean("is_verified").default(false),
   isActive: boolean("is_active").default(true),
-  createdAt: timestamp("created_at").defaultNow(),
-  updatedAt: timestamp("updated_at").defaultNow(),
+  ...timestamps,
 });
