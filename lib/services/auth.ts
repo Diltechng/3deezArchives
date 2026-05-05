773ecaf301464f73a3eef5e991a133fe3e042058
Diltechng <diltechng@gmail.com>
2026-05-05 11:39:27 +0100

    feature: db types

--- lib/services/auth.ts
+++ lib/services/auth.ts
@@ -1,5 +1,4 @@
 import { db } from "@/db";
-import { users, verificationTokens } from "@/db/schema/users/users";
 import { generateRandomString, generateHash } from "../crypto";
 import { RegisterInput } from "../validation/register.schema";
 import { VerifyEmailInput } from "../validation/verify-email.schema";
@@ -7,23 +6,26 @@ import { eq } from "drizzle-orm";
 import { VerificationError } from "../errors";
 import dayjs from "dayjs";
 import duration from "dayjs/plugin/duration";
+import { users, verificationTokens } from "@/db/schema/users";
 
 dayjs.extend(duration);
-
 
 export async function registerUser(data: RegisterInput) {
   const verificationToken = generateRandomString();
 
   await db.transaction(async (tx) => {
-    const [user] = await tx.insert(users).values({
-      email: data.email,
-      name: data.fullName,
-    }).returning();  
+    const [user] = await tx
+      .insert(users)
+      .values({
+        email: data.email,
+        name: data.fullName,
+      })
+      .returning();
 
     await tx.insert(verificationTokens).values({
       userId: user.id,
       tokenHash: generateHash(verificationToken),
-      expiresAt: new Date(dayjs.duration(1, "hour").asMilliseconds())
+      expiresAt: new Date(dayjs.duration(1, "hour").asMilliseconds()),
     });
   });
 
@@ -31,18 +33,21 @@ export async function registerUser(data: RegisterInput) {
 }
 
 export async function verifyEmail(userId: string, data: VerifyEmailInput) {
-  await db.transaction(async tx => {
-    const result = await tx.select({}).from(verificationTokens)
+  await db.transaction(async (tx) => {
+    const result = await tx
+      .select({ users, verificationTokens })
+      .from(verificationTokens)
+      .innerJoin(users, eq(verificationTokens.userId, users.id))
+      .where(eq(verificationTokens.userId, userId));
 
     const [record] = result;
-    if (!record)
-      throw new VerificationError("Invalid Verification Token");
+    if (!record) throw new VerificationError("Invalid Verification Token");
 
     const user = record.users;
-    const verificationToken = record.verification_tokens;
+    const verificationToken = record.verificationTokens;
 
     if (verificationToken.expiresAt >= new Date()) {
-
+      throw new VerificationError("Verification Token has expired");
     }
 
     await tx
