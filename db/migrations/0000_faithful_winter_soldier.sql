4de4283ba13889983e401abd7c5cf6177ce40af6
Ghali Musa <ghalimusa53@gmail.com>
2026-05-06 14:52:03 +0100

    fix: remove faulty migrations

--- db/migrations/0000_faithful_winter_soldier.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "users" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"email" text NOT NULL,
-	"name" text NOT NULL,
-	"is_active" boolean DEFAULT true,
-	"created_at" timestamp DEFAULT now(),
-	"updated_at" timestamp DEFAULT now(),
-	CONSTRAINT "users_email_unique" UNIQUE("email")
-);
