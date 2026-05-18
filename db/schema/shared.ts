d006c159cc2c660a52db153251bec0959a640da6
Ghali Musa <ghalimusa53@gmail.com>
2026-05-18 10:01:46 +0100

    feat: create user roles enum and invitations table

--- db/schema/shared.ts
+++ db/schema/shared.ts
@@ -1,6 +1,8 @@
-import { timestamp } from "drizzle-orm/pg-core";
+import { pgEnum, timestamp } from "drizzle-orm/pg-core";
 
 export const timestamps = {
   createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
   updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow()
 }
+
+export const userRoleEnum = pgEnum("user_role", ["admin", "staff"]);
--- db/schema/users/index.ts
+++ db/schema/users/index.ts
@@ -1,3 +1,3 @@
 export * from './user';
-export * from './token';
 export * from './session';
+export * from "./invitations";
