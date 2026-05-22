a3545c2e6571cfb821ea7ed3f6b68a7d01e163d5
Ghali Musa <ghalimusa53@gmail.com>
2026-05-22 12:12:19 +0100

    feat: add posts update service and zod schemas

--- shared/schemas/gallery/update-post.schema.ts
+++ shared/schemas/gallery/update-post.schema.ts
@@ -1,3 +1,16 @@
 import z from "zod";
+import { CategoryIdSchema, MediaIdArraySchema, MediaIdSchema, PostDateOfMomentSchema, PostDescriptionSchema, PostIdSchema, PostTagsSchema, PostTitleSchema, PostVisibilitySchema } from "../shared";
 
-export const UpdatePostSchema = z.object({});
+export const UpdatePostSchema = z.object({
+  id: PostIdSchema,
+  title: PostTitleSchema.optional(),
+  description: PostDescriptionSchema.optional(),
+  tags: PostTagsSchema.optional(),
+  coverMediaId: MediaIdSchema.optional(),
+  mediaIds: MediaIdArraySchema.optional(),
+  visibility: PostVisibilitySchema.optional(),
+  dateOfMoment: PostDateOfMomentSchema.optional(),
+  categoryId: CategoryIdSchema.optional(),
+});
+
+export type UpdatePostInput = z.infer<typeof UpdatePostSchema>;
