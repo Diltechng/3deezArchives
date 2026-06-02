5cfe28fc7811ccc99efabd9bbc2a066106704674
Ghali Musa <ghalimusa53@gmail.com>
2026-06-02 16:43:52 +0100

    feat: use useQuery to fetch posts

--- app/(protected)/gallery/page.tsx
+++ app/(protected)/gallery/page.tsx
@@ -1,9 +1,34 @@
 "use client"
 import CreatePostModal from "@/features/posts/components/CreatePostModal";
 import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { useAuthFetch } from "@/features/auth/hooks/useAuthFetch";
 
 const GalleryPage = () => {
   const [showUploadModal, setShowUploadModal] = useState(false);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
+
+  const authFetch = useAuthFetch();
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["posts"],
+    queryFn: async (): Promise<{ id: string; }[]> => {
+      const response = await authFetch(`/api/v1/gallery/posts?limit=${limit}&page=${page}`);
+      const data = await response.json();
+      return data.data;
+    }
+  });
+
+  console.log(data);
+
+  if (error) {
+    return <p>{error.message}</p>
+  }
+
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
 
   return (
     <section>
@@ -20,8 +45,8 @@ const GalleryPage = () => {
         </button>
         {showUploadModal && <CreatePostModal onExit={() => setShowUploadModal(false)} />}
       </header>
-      <div>
-
+      <div className="grid grid-cols-4">
+        {data && data.map(post => <div key={post.id}>{post.id}</div>)}
       </div>
     </section>
   )
