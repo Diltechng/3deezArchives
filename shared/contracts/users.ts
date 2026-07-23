81fecbefe3859fcb66f1e094bee9cba1fe18ad3b
Ghali Musa <ghalimusa53@gmail.com>
2026-07-23 07:08:45 +0100

    feat: define api contract for user profile

--- shared/contracts/users.ts
+++ shared/contracts/users.ts
@@ -1,6 +1,7 @@
 import { UserRole, UserStatus } from "../constants/enums";
 import { ResponseData, SerializeDates } from "../types/api";
 import { Pagination } from "../types/pagination";
+import { TResponse } from "./Response";
 
 export interface GetUsersMeta {
   pagination: Pagination;
@@ -16,5 +17,14 @@ export interface UserListItem {
  status: UserStatus;
  postsCount: number;
 };
+
+export type UserProfileData = {
+  id: string;
+  name: string;
+  email: string;
+  role: UserRole;
+}
+
+export type GetUserProfileResponse = TResponse<UserProfileData>
 
 export type GetUsersResponse = ResponseData<UserListItem[], GetUsersMeta>;
