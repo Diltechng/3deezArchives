08669870cc8c9646286356a9db6a9d27939adee8
Ghali Musa <ghalimusa53@gmail.com>
2026-07-23 11:32:39 +0100

    fix: configure invitation mailing service to use enviroment specified frontend url

--- modules/mailing/mail.service.ts
+++ modules/mailing/mail.service.ts
@@ -1,19 +1,25 @@
 import nodemailer from "nodemailer";
 import { SendInvitationEmailInput } from "./mailing.types";
 import { renderCompanyInviteEmail } from "@/modules/utils/email";
+import { InternalServerError } from "@/lib/errors";
 
 
 class MailService {
   async sendInvitationEmail(data: SendInvitationEmailInput) {
     const email = process.env.GOOGLE_MAIL_USER;
     const appPassword = process.env.GOOGLE_APP_PASSWORD;
+    const frontedUrl = process.env.FRONTEND_URL;
     const service = "gmail";
+
+    if (!email || !appPassword || !frontedUrl) {
+      throw new InternalServerError();
+    }
 
     const html = renderCompanyInviteEmail({
       companyName: "3Deez Global Investment",
       invitedByEmail: data.inviterEmail,
       invitedByUsername: data.inviterName,
-      inviteLink: `http://localhost:3000/invitation/accept?token=${data.invitationJwt}`,
+      inviteLink: `${frontedUrl}/invitation/accept?token=${data.invitationJwt}`,
       inviteRole: data.inviteeRole,
     });
     
