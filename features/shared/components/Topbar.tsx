18e941edd7d91285133491281fbb7e496819b767
Ghali Musa <ghalimusa53@gmail.com>
2026-07-24 11:13:53 +0100

    feat: update top bar to display current user info

--- features/shared/components/Topbar.tsx
+++ features/shared/components/Topbar.tsx
@@ -1,21 +1,25 @@
 import { TextAlignStart } from "lucide-react";
 import { usePathname } from "next/navigation";
 import useSidebar from "../hooks/useSidebar";
+import { useCurrentUser } from "@/features/users/hooks/useCurrentUser";
+import { cn, getInitials } from "../lib/utils";
 
 const Topbar = () => {
   const { toggleDesktop, toggleMobile } = useSidebar();
   const pathName = usePathname();
   const routeMap: Record<string, string> = {
-    "/dashboard": "archives",
+    "/dashboard": "Dashboard",
     "/activity-logs": "Activity Logs",
     "/gallery": "Gallery",
     "/users": "Users",
     "/users/invitations": "User Invitations",
     "/settings": "Settings",
-    "/settings/general": "General Settings",
+    "/settings/account": "Account",
   }
   
   const pageName = routeMap[pathName];
+  const { user } = useCurrentUser();
+  const isAdmin = user?.role === "admin";
 
   return (
     <header className="flex px-3.5 py-1.5 justify-between border-b border-border bg-surface-2">
@@ -40,11 +44,14 @@ const Topbar = () => {
         </span>
       </div>
       <div className="flex items-center justify-center gap-3">
-        <div className="px-2 py-0.75 rounded-sm text-[9px] tracking-[0.08rem] text-accent bg-accent/20 border border-accent">
-          ADMIN
+        <div className={cn(
+          "px-2 py-0.75 rounded-sm text-[9px] tracking-[0.08rem] uppercase border text-accent-3 bg-accent-3/20 border-accent-3",
+          { "text-accent bg-accent/20 border-accent": isAdmin }
+        )}>
+          {user?.role}
         </div>
         <div className="flex items-center justify-center rounded-full w-7 aspect-square text-[10px] border border-border-2 bg-surface-3 text-text-2">
-          DA
+          {user && getInitials(user?.name)}
         </div>
       </div>
     </header>
