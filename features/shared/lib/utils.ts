5bd0ce36723d39f6e58e756e24d110a325aa510a
Ghali Musa <ghalimusa53@gmail.com>
2026-07-29 13:46:13 +0100

    feat: add utils for accent functionalities

--- features/shared/lib/utils.ts
+++ features/shared/lib/utils.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { Accent } from "../types/accent.types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -25,3 +26,70 @@ export function getInitials(fullName: string) {
 
   return initials;
 }
+
+const accentClasses: Record<
+  Accent,
+  {
+    solid: string;
+    text: string;
+    faded: string;
+  }
+> = {
+  primary: {
+    solid: "bg-accent-primary text-background",
+    text: "text-accent-primary",
+    faded: "bg-accent-primary/10",
+  },
+  secondary: {
+    solid: "bg-accent-secondary",
+    text: "text-accent-secondary",
+    faded: "bg-accent-secondary/10"
+  },
+  neutral: {
+    solid: "bg-accent-neutral",
+    text: "text-accent-neutral",
+    faded: "bg-accent-neutral/10"
+  },
+  info: {
+    solid: "bg-accent-info",
+    text: "text-accent-info",
+    faded: "bg-accent-info/10"
+  },
+  danger: {
+    solid: "bg-accent-danger",
+    text: "text-accent-danger",
+    faded: "bg-accent-danger/10"
+  }
+}
+
+export function accentSolidCn(accent: Accent) {
+  return accentClasses[accent].solid;
+}
+
+export function accentTextCn(accent: Accent) {
+  return accentClasses[accent].text;
+}
+
+export function accentFadedCn(accent: Accent) {
+  return accentClasses[accent].faded;
+}
+
+const accents: Accent[] = [
+  "primary",
+  "secondary",
+  "info",
+]
+
+function hashString(str: string) {
+  let hash = 0
+
+  for (let i = 0; i < str.length; i++) {
+    hash = (hash * 31 + str.charCodeAt(i)) | 0
+  }
+
+  return Math.abs(hash)
+}
+
+
+export const accentFromName = (name: string): Accent =>
+  accents[hashString(name) % accents.length]
---
+++ features/shared/types/accent.types.ts
@@ -0,0 +1,6 @@
+export type Accent =
+  | "primary"
+  | "secondary"
+  | "neutral"
+  | "info"
+  | "danger"
