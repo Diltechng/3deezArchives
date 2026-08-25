bbe09cd9d26e016697f425ba0314628864777fac
Ghali Musa <ghalimusa53@gmail.com>
2026-08-25 19:49:22 +0100

    feat: add forwardRef to button component

--- features/common/ui/Button.tsx
+++ features/common/ui/Button.tsx
@@ -1,60 +1,54 @@
+import { forwardRef } from "react";
 import { cn } from "../lib/utils";
 import { Slot } from "radix-ui";
 
-interface ButtonProps {
+interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {
   children?: React.ReactNode;
   Icon?: React.ComponentType<{ className: string }>;
   active?: boolean;
   asChild?: boolean;
-  variant?: "contained" | "text" | "outlined"
-  className?: string;
-  disabled?: boolean;
-  form?: string;
-  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  variant?: "contained" | "text" | "outlined";
 };
 
-const Button = ({
-  className,
-  variant="contained",
-  form,
-  active=false,
-  asChild=false,
-  disabled=false,
-  Icon,
-  children,
-  onClick,
-  ...props
-}: ButtonProps) => {
-  const Comp = asChild? Slot.Root: "button";
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({
+    className,
+    variant="contained",
+    active=false,
+    asChild=false,
+    Icon,
+    children,
+    ...props
+  }, ref) => {
+    const Comp = asChild? Slot.Root: "button";
 
-  return (
-    <Comp
-      {...props}
-      form={form}
-      disabled={disabled}
-      onClick={onClick}
-      className={cn(
-        "flex items-center gap-2 py-2 px-4 text-[13px] text-left rounded-lg duration-200",
-        
-        variant === "contained" && (
-          "font-semibold bg-accent-primary text-background"
-        ),
+    return (
+      <Comp
+        ref={ref}
+        {...props}
+        className={cn(
+          "flex items-center gap-2 py-2 px-4 text-[13px] text-left rounded-lg duration-200",
+          
+          variant === "contained" && (
+            "font-semibold bg-accent-primary text-background"
+          ),
 
-        variant === "outlined" && (
-          "border border-border hover:bg-surface"
-        ),
+          variant === "outlined" && (
+            "border border-border hover:bg-surface"
+          ),
 
-        variant === "text" && cn(active
-          ? "bg-accent-primary/10 text-accent-primary"
-          : "text-foreground-secondary hover:text-text hover:bg-surface"
-        ),
-        className,
-      )}
-    >
-      {Icon && <Icon className="h-5 w-5" />}
-      <Slot.Slottable>{children}</Slot.Slottable>
-    </Comp>
-  )
-}
+          variant === "text" && cn(active
+            ? "bg-accent-primary/10 text-accent-primary"
+            : "text-foreground-secondary hover:text-text hover:bg-surface"
+          ),
+          className,
+        )}
+      >
+        {Icon && <Icon className="h-5 w-5" />}
+        <Slot.Slottable>{children}</Slot.Slottable>
+      </Comp>
+    )
+  }
+);
 
 export default Button;
