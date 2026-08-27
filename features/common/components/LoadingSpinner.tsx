f46c88b8b97cbb81171a277365179f59af300470
Ghali Musa <ghalimusa53@gmail.com>
2026-08-27 17:12:24 +0100

    feat: allow loading spinner to accept class name prop

--- features/common/components/LoadingSpinner.tsx
+++ features/common/components/LoadingSpinner.tsx
@@ -1,6 +1,17 @@
-const LoadingSpinner = ({ radius=10, width=3 }) => (
+import { cn } from "../lib/utils";
+
+interface LoadingSpinnerProps {
+  className?: string;
+  radius?: number;
+  width?: number;
+}
+
+const LoadingSpinner = ({ className, radius=10, width=3 }: LoadingSpinnerProps) => (
   <div
-    className="rounded-full border border-border-2 border-t-accent-primary animate-spin"
+    className={cn(
+      "rounded-full border border-border-2 border-t-accent-primary animate-spin",
+      className
+    )}
     style={{
       height: `${radius*4}px`,
       width: `${radius*4}px`,
