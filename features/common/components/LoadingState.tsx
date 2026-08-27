cc840dc5a9bf0d78477c1baaf11022dd09b9b1c6
Ghali Musa <ghalimusa53@gmail.com>
2026-08-27 17:00:58 +0100

    feat: allow loading state to accept children

--- features/common/components/LoadingState.tsx
+++ features/common/components/LoadingState.tsx
@@ -3,16 +3,17 @@ import { LoadingSpinner } from "./LoadingSpinner";
 
 interface LoadingStateProps {
   isFullScreen?: boolean;
+  children?: React.ReactNode;
 };
 
-const LoadingState = ({ isFullScreen }: LoadingStateProps) => (
+const LoadingState = ({ isFullScreen, children }: LoadingStateProps) => (
   <div
     className={cn(
       "flex justify-center items-center h-full w-full",
       { "fixed inset-0": isFullScreen }
     )}
   >
-    <LoadingSpinner />
+    {children ?? <LoadingSpinner />}
   </div>
 );
 
