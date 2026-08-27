fc8cfc6c0de9a9e41443906007cffa15324e2e7e
Ghali Musa <ghalimusa53@gmail.com>
2026-08-27 15:19:32 +0100

    feat: add fullscreen support for loading state component

--- features/common/components/LoadingState.tsx
+++ features/common/components/LoadingState.tsx
@@ -1,7 +1,17 @@
+import { cn } from "../lib/utils";
 import { LoadingSpinner } from "./LoadingSpinner";
 
-const LoadingState = () => (
-  <div className="flex justify-center items-center h-full w-full">
+interface LoadingStateProps {
+  isFullScreen?: boolean;
+};
+
+const LoadingState = ({ isFullScreen }: LoadingStateProps) => (
+  <div
+    className={cn(
+      "flex justify-center items-center h-full w-full",
+      { "fixed inset-0": isFullScreen }
+    )}
+  >
     <LoadingSpinner />
   </div>
 );
