1adf0efc5055398fc344e7f4e01157a94fe4c269
Ghali Musa <ghalimusa53@gmail.com>
2026-09-04 22:00:49 +0100

    feat: create types for db transaction

--- db/types.ts
+++ db/types.ts
@@ -1,4 +1,11 @@
-import { NodePgDatabase } from "drizzle-orm/node-postgres";
+import { NodePgDatabase, NodePgQueryResultHKT } from "drizzle-orm/node-postgres";
 import { DbSchema } from "./schema";
+import { PgTransaction } from "drizzle-orm/pg-core";
+import { ExtractTablesWithRelations } from "drizzle-orm";
 
 export type DbClient = NodePgDatabase<DbSchema>;
+export type DbTransaction = PgTransaction<
+  NodePgQueryResultHKT,
+  DbSchema,
+  ExtractTablesWithRelations<DbSchema>
+>;
