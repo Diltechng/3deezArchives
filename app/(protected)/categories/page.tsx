7c007e174b7ba54389b15a20eb441ca8eb5742e9
Ghali Musa <ghalimusa53@gmail.com>
2026-09-09 18:21:40 +0100

    refactor: refine categories page

--- app/(protected)/categories/page.tsx
+++ app/(protected)/categories/page.tsx
@@ -1,32 +1,28 @@
 "use client"
 import { PageHeader } from "@/features/common/components/PageHeader"
+import { useDateFilters } from "@/features/common/hooks/useDateFilters";
 import { useQueryParams } from "@/features/common/hooks/useQueryParams";
 import { Button } from "@/features/common/ui/Button";
 import { DropdownMenu, DropdownMenuArrow, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/features/common/ui/Dropdown";
 import { Input } from "@/features/common/ui/Input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/features/common/ui/Select";
-import { ArrowUpDown, ListFilter, Search } from "lucide-react";
+import { ArrowUpDown, ListFilter, Plus, Search } from "lucide-react";
 import { useDebouncedCallback } from "use-debounce";
 
 type QueryParams = {
-  search: string | null;
-  from: string | null;
-  to: string | null;
-  status: string | null;
-  sortBy: string | null;
+  search?: string;
+  from?: string;
+  to?: string;
+  status?: string;
+  sortBy?: string;
 }
 
 const CategoriesPage = () => {
-  const { params: queryParams, updateQueryParams } = useQueryParams<QueryParams>({
-    search: null,
-    from: null,
-    to: null,
-    status: null,
-    sortBy: null,
-  });
-
+  const { params: queryParams, updateQueryParams } = useQueryParams<QueryParams>();
+  
   const { search, from: dateFrom, to: dateTo, status, sortBy } = queryParams;
-
+  const { dateOptions, getDateFilterValue } = useDateFilters({ from: dateFrom, to: dateTo });
+  
   const handleSearch = useDebouncedCallback(
     (term: string) => {
       updateQueryParams({ search: term ?? null });
@@ -45,88 +41,10 @@ const CategoriesPage = () => {
     { id: "all", name: "All", slug: "all" },
   ];
 
-  const dateFilters = [
-    {
-      name: "Yesterday",
-      value: "yesterday",
-      range() {
-        const now = new Date();
-        const yesterday = new Date(now);
-        yesterday.setDate(now.getDate() - 1);
-
-        return {
-          from: yesterday.toISOString().split("T")[0],
-          to: now.toISOString().split("T")[0],
-        }
-      }
-    },
-    {
-      name: "Last Week",
-      value: "last_week",
-      range() {
-        const now = new Date();
-        const lastWeek = new Date(now);
-        lastWeek.setDate(now.getDate() - 7);
-
-        return {
-          from: lastWeek.toISOString().split("T")[0],
-          to: now.toISOString().split("T")[0]
-        }
-      }
-    },
-    {
-      name: "Last Month",
-      value: "last_month",
-      range() {
-        const now = new Date();
-        const lastMonth = new Date(now);
-        lastMonth.setMonth(now.getMonth() - 1);
-
-        return {
-          from: lastMonth.toISOString().split("T")[0],
-          to: now.toISOString().split("T")[0]
-        }
-      }
-    },
-    {
-      name: "Last Year",
-      value: "last_year",
-      range() {
-        const now = new Date();
-        const lastYear = new Date(now);
-        lastYear.setFullYear(now.getFullYear() - 1);
-
-        return {
-          from: lastYear.toISOString().split("T")[0],
-          to: now.toISOString().split("T")[0]
-        }
-      }
-    },
-    {
-      name: "Custom Range",
-      value: "custom_range",
-      range() {
-        return {
-          from: dateFrom ?? "",
-          to: dateTo ?? ""
-        }
-      }
-    }
-  ];
-
   const sortOptions: { name: string; value: "latest" | "oldest"; }[] = [
     { name: "Latest", value: "latest" },
     { name: "Oldest", value: "oldest" }
   ];
-
-  function getDateFilterValue(date: { from: string; to: string; }) {
-    return dateFilters.find(filter => {
-      const range = filter.range();
-
-      return range.from === date.from &&
-        range.to === date.to;
-    })?.value || "custom_range"
-  }
 
   function updateDateQueryParams(value: string) {
     if (value === "all") {
@@ -135,7 +53,7 @@ const CategoriesPage = () => {
         to: null
       });
     } else {
-      const filter = dateFilters.find(f => f.value === value);
+      const filter = dateOptions.find(f => f.value === value);
       if (!filter) return;
 
       const range = filter.range();
@@ -148,7 +66,12 @@ const CategoriesPage = () => {
 
   return (
     <div>
-      <PageHeader title="Categories" subtitle="" />
+      <PageHeader title="Categories" subtitle="">
+        <Button>
+          <Plus className="size-4" />
+          Add Category
+        </Button>
+      </PageHeader>
       <div className="flex gap-2">
         <div className="relative flex-1">
           <Search className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-foreground-secondary" />
@@ -205,7 +128,7 @@ const CategoriesPage = () => {
                 <SelectItem value="all">
                   All Time
                 </SelectItem>
-                {dateFilters.map(filter => (
+                {dateOptions.map(filter => (
                   <SelectItem key={filter.name} value={filter.value}>
                     {filter.name}
                   </SelectItem>
