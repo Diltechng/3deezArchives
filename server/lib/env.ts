02b5f460399df1d08cb69729e298b8d3ef6c6eab
Ghali Musa <ghalimusa53@gmail.com>
2026-09-16 17:01:51 +0100

    feat: make cloundary upload folder environment variable config optional

--- server/lib/env.ts
+++ server/lib/env.ts
@@ -20,7 +20,7 @@ const EnvSchema = z.object({
   CLOUDINARY_CLOUD_NAME: z.string().min(1),
   CLOUDINARY_API_KEY: z.string().min(1),
   CLOUDINARY_API_SECRET: z.string().min(1),
-  CLOUDINARY_UPLOAD_FOLDER: z.string().min(1),
+  CLOUDINARY_UPLOAD_FOLDER: z.string().min(1).optional(),
 });
 
 export const env = EnvSchema.parse(process.env);
