b77b2ffd0338e25fde8a2d37bf08b81b2c43ea41
Ghali Musa <ghalimusa53@gmail.com>
2026-09-16 17:08:07 +0100

    feat: make all env variables optional in env config

--- server/lib/env.ts
+++ server/lib/env.ts
@@ -1,20 +1,20 @@
 import z from "zod"
 
 const EnvSchema = z.object({
-  NODE_ENV: z.enum(["development", "production", "test"]),
-  FRONTEND_URL: z.url().default("http://localhost:3000"),
+  NODE_ENV: z.enum(["development", "production", "test"]).optional(),
+  FRONTEND_URL: z.url().default("http://localhost:3000").optional(),
 
   SUPER_ADMIN_NAME: z.string().min(1).default("Super Admin"),
   SUPER_ADMIN_EMAIL: z.email().default("super@admin.com"),
   SUPER_ADMIN_PASSWORD: z.string().min(1).default("admin123"),
 
-  DATABASE_URL: z.url(),
+  DATABASE_URL: z.url().optional(),
 
-  GOOGLE_MAIL_USER: z.email(),
-  GOOGLE_APP_PASSWORD: z.string().min(1),
+  GOOGLE_MAIL_USER: z.email().optional(),
+  GOOGLE_APP_PASSWORD: z.string().min(1).optional(),
 
-  JWT_SECRET: z.string().min(1),
-  INVITATION_JWT_SECRET: z.string().min(1),
+  JWT_SECRET: z.string().min(1).optional(),
+  INVITATION_JWT_SECRET: z.string().min(1).optional(),
   CRON_SECRET: z.string().min(1).optional(),
 
   CLOUDINARY_CLOUD_NAME: z.string().min(1),
